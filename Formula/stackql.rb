ba976b7d84124d697d9b1680183e81689e178da9
General Kroll <generalkroll0@gmail.com>
2023-02-13 00:15:00 +1100

    v-0-3-293

--- Formula/stackql.rb
+++ Formula/stackql.rb
@@ -2,9 +2,9 @@ class Stackql < Formula
   desc "SQL interface for arbitrary resources with full CRUD support"
   homepage "https://stackql.io/"
   url "https://github.com/stackql/stackql.git",
-    tag:      "v0.3.265",
-    revision: "ce58a3b9cd00022da42cb8144922613e9491f2f3"
-  sha256 "46d00d6bc2c1e2d3351a10d0b753df0c0e87f9538f058cfc69f4f886ceac8725"
+    tag:      "v0.3.293",
+    revision: "d739e964a3f7ab917d774dd3fb6beb091abf2342"
+  sha256 "ca28f4d8e4f0e90e03d939b21533a30b4b1fb8ef7fe3622c0587e43431dc2925"
   license "MIT"
 
   depends_on "go@1.19" => :build
@@ -16,7 +16,7 @@ class Stackql < Formula
       "-w",
       "-X github.com/stackql/stackql/internal/stackql/cmd.BuildMajorVersion=0",
       "-X github.com/stackql/stackql/internal/stackql/cmd.BuildMinorVersion=3",
-      "-X github.com/stackql/stackql/internal/stackql/cmd.BuildPatchVersion=265",
+      "-X github.com/stackql/stackql/internal/stackql/cmd.BuildPatchVersion=293",
       "-X github.com/stackql/stackql/internal/stackql/cmd.BuildCommitSHA=#{Utils.git_head}",
       "-X github.com/stackql/stackql/internal/stackql/cmd.BuildShortCommitSHA=#{Utils.git_short_head}",
       "-X \"stackql/internal/stackql/planbuilder.PlanCacheEnabled=true\"",
